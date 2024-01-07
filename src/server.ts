2d2d25a7a4e4c64c3e9fb908091b3583da2a4aad
Devair <devairft@gmail.com>
2024-01-07 17:38:26 -0300

    category mapping (#12)

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,15 @@ import "express-async-errors";
 import { router } from './routers'
 
 import './shared/container';
+import './shared/infra/typeorm';
+
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (request, response)=>{
-    return response.json({messaege: 'Wello world'})
+    return response.json({message: 'Wello world'})
 })
 
 app.use(router)
