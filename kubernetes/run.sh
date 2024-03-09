3ec841b98d4aeb4c13cae839a9fead740293014f
Devair <devairft@gmail.com>
2024-03-09 20:01:12 +0000

    shell script

---
+++ kubernetes/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Cleaning old Pods"
+kubectl delete pods --all
+
+echo "Deploying ConfigMaps"
+kubectl apply -f cm-app.yaml        
+kubectl apply -f cm-postgres.yaml   
+
+echo "Deploying Volume"
+kubectl apply -f pv-postgres.yaml   
+kubectl apply -f vc-postgres.yaml
+
+echo "Deploying Pods"
+kubectl apply -f pod-app.yaml
+kubectl apply -f pod-postgres.yml   
+
+echo "Deploying Services"
+
+kubectl apply -f svc-postgres.yaml
+kubectl apply -f svc-app.yaml
+
